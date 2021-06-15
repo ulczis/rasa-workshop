9792d740b4069473ad50ad31a74b670caae79390
Paweł A. Ryszawa <pablo53@poczta.onet.eu>
2021-06-15 19:14:50 +0200

    ask incident status

--- assistant/actions/actions.py
+++ assistant/actions/actions.py
@@ -2,6 +2,7 @@ import logging
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import AllSlotsReset, SlotSet, EventType, SessionStarted, ActionExecuted
+import pandas as pd
 
 class IncidentStatus(Action):
 
@@ -9,5 +10,9 @@ class IncidentStatus(Action):
         return 'action_incident_status'
 
     def run(self, dispatcher, tracker, domain):
-        dispatcher.utter_message("The status of your ticket is : pending")
+        data = pd.read_csv('workshop_data.csv')
+        user_id = tracker.get_slot('number')
+        find_user = data.loc[data['user_id']==user_id]
+        status = find_user.incident_number_status.values[0]
+        dispatcher.utter_message("The status of your ticket is : {}".format(status))
         return []
